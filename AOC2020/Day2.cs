7d00cc577a586ca0052313ee1df3ab8cb4644094
Vincent Duong <vduong@relias.com>
2020-12-02 12:40:22 -0500

    Day 2 B

--- AOC2020/Day2.cs
+++ AOC2020/Day2.cs
@@ -36,9 +36,33 @@ namespace AOC2020
                         break;
                 }
 
-                if (dict.ContainsKey(character) && 
-                    dict[character] >= min && 
+                if (dict.ContainsKey(character) &&
+                    dict[character] >= min &&
                     dict[character] <= max)
+                    validPasswords++;
+            }
+
+            return validPasswords;
+        }
+
+        public static int Day2B()
+        {
+            var validPasswords = 0;
+
+            AOCInput input = new AOCInput().GetInput("Inputs/Day2A.json");
+
+            foreach (var item in input.InputString)
+            {
+                // "2-4 p: vpkpp"
+                var arr = item.Split(" ");
+                var range = arr[0].Split("-");
+                var idxA = int.Parse(range[0]) - 1;
+                var idxB = int.Parse(range[1]) - 1;
+                var character = arr[1][0];
+                var password = arr[2];
+                
+                if ((password[idxA] == character && password[idxB] != character) ||
+                    (password[idxA] != character && password[idxB] == character))
                     validPasswords++;
             }
 
--- AOC2020/Program.cs
+++ AOC2020/Program.cs
@@ -9,6 +9,7 @@ namespace AOC2020
             Console.WriteLine($"Day 1A - {Day1.Day1A()}");
             Console.WriteLine($"Day 1B - {Day1.Day1B()}");
             Console.WriteLine($"Day 2A - {Day2.Day2A()}");
+            Console.WriteLine($"Day 2B - {Day2.Day2B()}");
         }
     }
 }
