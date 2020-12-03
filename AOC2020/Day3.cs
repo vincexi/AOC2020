13ed7a58035d5a97298949a827714a0107ba4575
Vincent Duong <vduong@relias.com>
2020-12-03 10:06:50 -0500

    Day 3 A

---
+++ AOC2020/Day3.cs
@@ -0,0 +1,29 @@
+﻿using AOC2020.Inputs;
+
+namespace AOC2020
+{
+    public static class Day3
+    {
+        public static int Day3A()
+        {
+            var trees = 0;
+
+            AOCInput input = new AOCInput().GetInput("Inputs/Day3A.json");
+            
+            int i = 0;
+            int j = 0;
+
+            while(i < input.InputString.Count)
+            {
+                if (input.InputString[i][j] == '#')
+                    trees++;
+                
+                i++;
+                j += 3;
+                j = j > input.InputString[0].Length - 1 ? j - input.InputString[0].Length : j;
+            }
+
+            return trees;
+        }
+    }
+}
--- AOC2020/Program.cs
+++ AOC2020/Program.cs
@@ -6,10 +6,11 @@ namespace AOC2020
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Day 1A - {Day1.Day1A()}");
-            Console.WriteLine($"Day 1B - {Day1.Day1B()}");
-            Console.WriteLine($"Day 2A - {Day2.Day2A()}");
-            Console.WriteLine($"Day 2B - {Day2.Day2B()}");
+            //Console.WriteLine($"Day 1A - {Day1.Day1A()}");
+            //Console.WriteLine($"Day 1B - {Day1.Day1B()}");
+            //Console.WriteLine($"Day 2A - {Day2.Day2A()}");
+            //Console.WriteLine($"Day 2B - {Day2.Day2B()}");
+            Console.WriteLine($"Day 3A - {Day3.Day3A()}");
         }
     }
 }
