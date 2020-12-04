15800024db30d62b68c731169615408aa6330723
vincexi <Vincent.Duong@fsresidential.com>
2020-12-04 01:02:56 -0500

    Day 4 A

---
+++ AOC2020/Day4.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AOC2020
+{
+    public static class Day4
+    {
+        public static int Day4A()
+        {
+            var validPassports = 0;
+            var totalPassports = 0;
+            var requiredKeys = new string[] { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+            string[] lines = File.ReadAllLines("Inputs/Day4A.txt");
+
+            var passport = string.Empty;
+
+            foreach (string line in lines)
+            {
+                if(!string.IsNullOrWhiteSpace(line))
+                {
+                    passport += $" {line}";
+                    continue;
+                }
+
+                var pairs = passport.Split(" ");
+                
+                var keys = pairs.Select(x => x.Split(":"))
+                    .Select(y=>y[0])
+                    .Where(z=> !string.IsNullOrWhiteSpace(z))
+                    .ToArray();
+
+                passport = string.Empty;
+                totalPassports++;
+
+                var isValid = true;
+
+                foreach (var item in requiredKeys)
+                {
+                    if(!keys.Contains(item))
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+
+                validPassports = isValid ? validPassports + 1 : validPassports;
+            }
+
+            Console.WriteLine($"Total passports {totalPassports}");
+            return validPassports;
+        }
+
+    }
+}
--- AOC2020/Program.cs
+++ AOC2020/Program.cs
@@ -10,8 +10,9 @@ namespace AOC2020
             //Console.WriteLine($"Day 1B - {Day1.Day1B()}");
             //Console.WriteLine($"Day 2A - {Day2.Day2A()}");
             //Console.WriteLine($"Day 2B - {Day2.Day2B()}");
-            Console.WriteLine($"Day 3A - {Day3.Day3A()}");
-            Console.WriteLine($"Day 3B - {Day3.Day3B()}");
+            //Console.WriteLine($"Day 3A - {Day3.Day3A()}");
+            //Console.WriteLine($"Day 3B - {Day3.Day3B()}");
+            Console.WriteLine($"Day 4A - {Day4.Day4A()}");
         }
     }
 }
