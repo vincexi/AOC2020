fe7238f368b64cb6c94b2885a3b265819dd5a770
vincexi <Vincent.Duong@fsresidential.com>
2020-12-06 22:21:55 -0500

    Day 6 A

---
+++ AOC2020/Day6.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AOC2020
+{
+    public static class Day6
+    {
+        public static int Day6A()
+        {
+            var count = 0;
+            string[] lines = File.ReadAllLines("Inputs/Day6A.txt");
+
+            var answers = string.Empty;
+
+            foreach (string line in lines)
+            {
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    answers += $"{line}";
+
+                    if (line != lines.Last())
+                        continue;
+                }
+
+                var counts = new HashSet<char>();
+
+                for (int i = 0; i < answers.Length; i++)
+                {
+                    if (!counts.Contains(answers[i]))
+                    {
+                        counts.Add(answers[i]);
+                        count++;
+                    }
+
+                }
+
+                answers = string.Empty;
+            }
+
+            return count;
+        }
+
+    }
+}
--- AOC2020/Program.cs
+++ AOC2020/Program.cs
@@ -8,14 +8,21 @@ namespace AOC2020
         {
             //Console.WriteLine($"Day 1A - {Day1.Day1A()}");
             //Console.WriteLine($"Day 1B - {Day1.Day1B()}");
+
             //Console.WriteLine($"Day 2A - {Day2.Day2A()}");
             //Console.WriteLine($"Day 2B - {Day2.Day2B()}");
+            
             //Console.WriteLine($"Day 3A - {Day3.Day3A()}");
             //Console.WriteLine($"Day 3B - {Day3.Day3B()}");
+            
             //Console.WriteLine($"Day 4A - {Day4.Day4A()}");
             //Console.WriteLine($"Day 4B - {Day4.Day4B()}");
-            Console.WriteLine($"Day 5A - {Day5.Day5A()}");
-            Console.WriteLine($"Day 5B - {Day5.Day5B()}");
+            
+            //Console.WriteLine($"Day 5A - {Day5.Day5A()}");
+            //Console.WriteLine($"Day 5B - {Day5.Day5B()}");
+            
+            Console.WriteLine($"Day 6A - {Day6.Day6A()}");
+            //Console.WriteLine($"Day 6B - {Day6.Day6B()}");
         }
     }
 }
